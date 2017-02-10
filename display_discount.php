673b436e47aa06876a23b5b0a3a068392cf6b89d
prajakta s lavangare <psl9@afsconnect1.njit.edu>
2017-02-10 14:23:57 -0500

    Formatted Currency and Discount

--- display_discount.php
+++ display_discount.php
@@ -9,6 +9,13 @@ $discount_percent = filter_input(INPUT_POST,'discount_percent');
 $discount_amount = $list_price * $discount_percent * 0.01;
 $discount_price = $list_price - $discount_amount;
 
+//Formatting Currency
+
+$list_price_formatted = "$".number_format($list_price,2);
+$discount_percent_formatted = $discount_percent."%";
+$discount_amount_formatted = "$".number_format($discount_amount,2);
+$discount_price_formatted = "$".number_format($discount_price,2);
+
 ?>
 
 
@@ -23,19 +30,19 @@ $discount_price = $list_price - $discount_amount;
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo $product_description;?></span><br>
+        <span><?php echo $product_description_formatted;?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo $list_price;?></span><br>
+        <span><?php echo $list_price_formatted;?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo $discount_percent;?></span><br>
+        <span><?php echo $discount_percent_formatted;?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo $discount_amount; ?></span><br>
+        <span><?php echo $discount_amount_formatted; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo $discount_price; ?></span><br>
+        <span><?php echo $discount_price_formatted; ?></span><br>
     </main>
 </body>
 </html>
